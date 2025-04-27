8786f1db89cbdf854d0b681ec022b88449941203
Mihir2423 <mihirraj444@gmail.com>
2025-04-27 07:56:14 +0530

    feat: complete home page ui

--- client/src/app/(routes)/home/page.tsx
+++ client/src/app/(routes)/home/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { CuboidIcon as Cube, FileSpreadsheet, Search } from "lucide-react";
 
 const HomePage = () => {
   return (
-    <div>Hi From Home</div>
-  )
-}
+    <div className="flex justify-center items-center w-full h-full">
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center gap-3">
+          <div className="bg-white hover:bg-neutral-100 p-3 rounded-lg">
+            <Cube className="w-8 h-8 text-black" />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h1 className="font-medium text-whtie text-xl">Blography</h1>
+            <p className="text-neutral-400 text-sm">
+              Write it down. Remember it forever.
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center gap-3">
+          <div className="flex flex-col gap-2 bg-[#262626] p-4 border border-[#404040] rounded-md w-[250px] cursor-pointer">
+            <FileSpreadsheet className="text-white" />
+            <p className="text-neutral-200 text-sm">New Document</p>
+          </div>
+          <div className="flex flex-col gap-2 bg-[#262626] p-4 border border-[#404040] rounded-md w-[250px] cursor-pointer">
+            <Search className="text-white" />
+            <p className="text-neutral-200 text-sm">Search</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
