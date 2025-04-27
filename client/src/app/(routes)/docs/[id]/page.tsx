db575d4811ee07a751fb6654b141ff54bda689c3
Mihir2423 <mihirraj444@gmail.com>
2025-04-27 09:19:07 +0530

    feat: add light bg

--- client/src/app/(routes)/docs/[id]/page.tsx
+++ client/src/app/(routes)/docs/[id]/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import { EditorSkeleton } from "./_components/editor-skeleton";
+import Image from "next/image";
 
 const Editor = dynamic(
   () => import("./_components/editor").then((mod) => mod.Editor),
@@ -19,7 +20,14 @@ const Docs = () => {
   };
 
   return (
-    <div className="py-6">
+    <div className="relative py-6">
+      <Image
+        src="/assets/images/light.webp"
+        alt="Light"
+        height={2048}
+        width={2048}
+        className="hidden md:block -top-20 right-0 left-0 z-0 absolute mx-auto w-screen max-w-screen h-screen max-h-screen pointer-events-none select-none"
+      />
       <div className="flex flex-col gap-1 pl-[69px]">
         <h1 className="font-semibold text-white text-3xl">New Document</h1>
         <p className="text-neutral-400 text-sm">Updated at: 27/04/2025</p>
